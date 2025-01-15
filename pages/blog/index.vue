cc0c44ab89c4184be9e86ea33c778db7bd568e43
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-01-15 23:50:04 +0900

    add dynamic route

---
+++ pages/blog/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Blog List</h1>
+  <ul>
+    <li><NuxtLink to="/blog/1">Blog 1</NuxtLink></li>
+    <li><NuxtLink to="/blog/2">Blog 2</NuxtLink></li>
+    <li><NuxtLink to="/blog/3">Blog 3</NuxtLink></li>
+    <li><NuxtLink to="/blog/4">Blog 4</NuxtLink></li>
+    <li><NuxtLink to="/blog/5">Blog 5</NuxtLink></li>
+  </ul>
+</template>
