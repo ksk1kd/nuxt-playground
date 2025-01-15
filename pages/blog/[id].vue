58d784384b056338ed7c982c5565040239b8bbb2
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-01-15 23:54:30 +0900

    add route validation

--- pages/blog/[id].vue
+++ pages/blog/[id].vue
@@ -1,4 +1,9 @@
 <script setup lang="ts">
+definePageMeta({
+  validate: async (route) => {
+    return typeof route.params.id === 'string' && /^\d+$/.test(route.params.id)
+  },
+})
 const route = useRoute()
 </script>
 
