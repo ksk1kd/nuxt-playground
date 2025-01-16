1acb8e0871378e1949c4ad131f7403107e42afa5
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-01-16 21:32:47 +0900

    config prerender

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+interface Post {
+  id: string
+  title: string
+  content: string
+}
+
 export default defineNuxtConfig({
+  hooks: {
+    async 'prerender:routes'(ctx) {
+      const posts: Post[] = await fetch('https://api.vercel.app/blog').then(
+        (res) => res.json(),
+      )
+      posts.map((post) => {
+        ctx.routes.add(`/blog/${post.id}`)
+      })
+    },
+  },
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
 })
